7a1ca01e22d6992de3f70631e42ef8dcdd974635
Osamu Monoe <osamum@microsoft.com>
2025-07-25 13:48:37 +0900

    Change RAG field name

--- AOAI/rag.js
+++ AOAI/rag.js
@@ -40,7 +40,7 @@ async function findIndex(queryText) {
             console.log('Found it in RAG data.');
             return '以下の [question] の内容に対し、[content]の内容を使用して回答してください。'
                 + 'ただし[question]に対し[content]の内容に回答に必要な情報がないと判断した場合は[content]の内容を無視して回答してください\n\n'
-                + `[question]\n${queryText}\n\n[content]\n${result.document.content}`;
+                + `[question]\n${queryText}\n\n[content]\n${result.document.chunk}`;
         } else {
             return queryText;
         }
