d469ef8f9ceaf4ec2bc67c2fbe28b3a1d629e277
Osamu Monoe <osamum@microsoft.com>
2025-08-28 09:50:16 +0900

    Use Key Vault for API keys

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
 //[REPLACE:use_keyVault]
+//Azure KeyVault からシークレットを取得して環境変数に設定(EntraID 使用の際は削除)
+const keyManager = require('./keymgr.js');
+keyManager.setSecretEnv();
+//---
 const rag = require('./AOAI/rag.js');
 const lm = require('./AOAI/lm.js');
 const webSearch = require('./AOAI/webSearch.js')
